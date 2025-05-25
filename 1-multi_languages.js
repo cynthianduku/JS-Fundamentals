7281d2c3d8594c14a3f453bf00995b8e3acd2352
cynthianduku <cynthiandukuu@gmail.com>
2025-05-25 16:01:47 +0300

    Fix capitalization for multi_languages output

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,6 +1,6 @@
-const line1 = "c is fun";
-const line2 = "python is cool";
-const line3 = "javascript is amazing";
+const line1 = "C is fun";
+const line2 = "Python is cool";
+const line3 = "Javascript is amazing";
 
 console.log(line1);
 console.log(line2);
